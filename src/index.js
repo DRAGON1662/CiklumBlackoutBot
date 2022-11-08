8236f07822589490f87bd671118eb0e31b6a3c26
yurii.fostiak <yurii.fostiak@mediamonks.com>
2022-11-08 22:29:58 +0200

    UI changes, test spreadsheet

--- src/index.js
+++ src/index.js
@@ -5,14 +5,47 @@ const TOKEN = process.env.TOKEN;
 
 const bot = new telegramBot(TOKEN, { polling: true });
 
-bot.on('message', (message) => {
+bot.on('message', async (message) => {
+    // let admin_id = 615570939;
+    // sticker id = CAACAgIAAxkBAAN5Y2qGcvh1ZnaHNrL8uL8bskrGmbIAAooBAAIrXlMLG0NsywABPrREKwQ
+
+    let admin_id = 396961963; //Yurii Fostiak ID
     let user_id = message.from.id;
     let user = message.from.first_name + ' ' + message.from.last_name;
     let user_message = message.text;
 
+
+
+    // if (user_message == '/start'){
+    //     await bot.sendMessage(user_id, `Привіт, ${user}! \nЯкщо в тебе вимкнули світло і ти не маєш змоги працювати певний час, то вкажи це тут.`)
+    //     await bot.sendMessage(user_id, 'Скільки часу тобі необхідно, щоб повернутись до роботи? \nВкажи проміжок часу (за Києвом), коли ти не активний(-a):', {
+    //         reply_markup: {
+    //             keyboard: [
+    //                 [{text: '10 - 11'}], 
+    //                 [{text: '11 - 12'}], 
+    //                 [{text: '12 - 13'}], 
+    //                 [{text: '14 - 15'}], 
+    //                 [{text: '15 - 16'}], 
+    //                 [{text: '17 - 18'}], 
+    //                 [{text: '18 - 19'}]
+    //             ]
+    //         }
+    //     })
+    //     // await bot.sendMessage(user_id, reply_markup.keyboard[0])
+    // } else if(user_message != '/start'){
+    //     // console.log(message);
+    //     await bot.sendMessage(user_id, 'Повідомлення надіслано!');
+    //     await bot.sendSticker(user_id, 'CAACAgIAAxkBAAN5Y2qGcvh1ZnaHNrL8uL8bskrGmbIAAooBAAIrXlMLG0NsywABPrREKwQ')
+    //     await bot.sendMessage(admin_id, `${user} буде відсутній у проміжку ${user_message}, за Київським часом. \nВідміть його в спредшиті`)
+    // }
     
     if (user_message == '/stop')
-        bot.sendMessage(user_id, 'U r trying to stop bot')
-    else
-        bot.sendMessage(user_id, `Hello, ${user}, from bot!`)
+        await bot.sendMessage(user_id, 'U r trying to stop bot')
+    
 })
+
+
+// Find sticker id
+// bot.on('sticker', function (msg) {
+//       console.log(msg.sticker.file_id)
+// });
